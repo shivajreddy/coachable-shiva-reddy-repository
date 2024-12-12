25884c2899771ca532093191045773122e82b30e
shiva <shivajreddy@outlook.com>
2024-12-11 20:38:34 -0500

    [ignore]

--- leetcode/microsoft/443_String_Comparison.cpp
+++ leetcode/microsoft/443_String_Comparison.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution
 {
 public:
-    int compress(vector<char> &chars) {}
+    int compress(vector<char> &chars)
+    {
+        int result = -1;
+        return result;
+    }
 };
 
 int main()
 {
+    Solution s;
+
     // TEST 1
     {
-        vector<char> chars = {}
+        vector<char> chars = { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+        cout << "RESULT  : " << s.compress(chars) << endl;
+        cout << "EXPECTED: " << 6 << "\n" << endl;
+    }
+    // TEST 2
+    {
+        vector<char> chars = { 'a' };
+        cout << "RESULT  : " << s.compress(chars) << endl;
+        cout << "EXPECTED: " << 1 << "\n" << endl;
+    }
+    // TEST 1
+    {
+        vector<char> chars = { 'a', 'b', 'b', 'b', 'b', 'b', 'b',
+                               'b', 'b', 'b', 'b', 'b', 'b' };
+        cout << "RESULT  : " << s.compress(chars) << endl;
+        cout << "EXPECTED: " << 4 << "\n" << endl;
     }
 
     return 0;
