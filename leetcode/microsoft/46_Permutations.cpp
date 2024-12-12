4b94b629d20d894227067dd6b0169fb20cbe2c63
shiva <shivajreddy@outlook.com>
2024-12-12 12:47:23 -0500

    [ignore]

---
+++ leetcode/microsoft/46_Permutations.cpp
@@ -0,0 +1,78 @@
+// 46. Permutations
+// https://leetcode.com/problems/permutations
+
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+Observations:
+*/
+
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    auto printMatrix = [&](const vector<vector<int>> &matrix) {
+        for (auto &v : matrix) {
+            for (auto &num : v) {
+                cout << num << " ";
+            }
+            cout << ", ";
+        }
+        cout << endl;
+    };
+
+    // Test Case 1
+    {
+        vector<int> nums = { 1, 2, 3 };
+        auto result = s.permute(nums);
+        cout << "RESULT  : ";
+        printMatrix(result);
+
+        vector<vector<int>> answer = { { 1, 2, 3 }, { 1, 3, 2 }, { 2, 1, 3 },
+                                       { 2, 3, 1 }, { 3, 1, 2 }, { 3, 2, 1 } };
+        cout << "EXPECTED: ";
+        printMatrix(answer);
+        cout << endl;
+    }
+    // Test Case 2
+    {
+        vector<int> nums = { 0, 1 };
+        auto result = s.permute(nums);
+        cout << "RESULT  : ";
+        printMatrix(result);
+
+        vector<vector<int>> answer = { { 0, 1 }, { 1, 0 } };
+        cout << "EXPECTED: ";
+        printMatrix(answer);
+        cout << endl;
+    }
+    // Test Case 3
+    {
+        vector<int> nums = { 1 };
+        auto result = s.permute(nums);
+        cout << "RESULT  : ";
+        printMatrix(result);
+
+        vector<vector<int>> answer = { { 1 } };
+        cout << "EXPECTED: ";
+        printMatrix(answer);
+        cout << endl;
+    }
+
+    return 0;
+}
