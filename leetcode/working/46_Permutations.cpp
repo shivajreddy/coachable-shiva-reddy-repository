66157956831768c1a8ff0de092d771224edd63ae
shiva <shivajreddy@outlook.com>
2025-01-20 17:48:41 -0500

    .

--- leetcode/working/46_Permutations.cpp
+++ leetcode/working/46_Permutations.cpp
@@ -11,19 +11,48 @@ using namespace std;
 Observations:
 
   example: 1, 2, 3
+
                 ___
       1__       2__         3__
   12_  13_    21_  23_    31_  32_
   123  132    213  231    312  321
 
-
-
 */
 
 class Solution {
+ private:
+  void backtrack(vector<int>& nums, vector<bool>& used,
+                 vector<int>& current_path, vector<vector<int>>& result) {
+    // Base Condition
+    if (current_path.size() == nums.size()) {
+      result.push_back(current_path);
+      return;
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+
+      // skip already used number
+      if (used[i])
+        continue;
+
+      used[i] = true;  // mark as read
+      current_path.push_back(nums[i]);
+      backtrack(nums, used, current_path, result);
+
+      current_path.pop_back();
+      used[i] = false;
+    }
+  }
+
  public:
   vector<vector<int>> permute(vector<int>& nums) {
+
     vector<vector<int>> result;
+    vector<bool> used(nums.size(), false);
+    vector<int> current_path;
+
+    backtrack(nums, used, current_path, result);
+
     return result;
   }
 };
