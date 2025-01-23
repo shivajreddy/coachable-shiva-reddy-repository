f0aaa46bf6aaf4d7a037b8a8fb48f2c333fe92d4
shiva <shivajreddy@outlook.com>
2025-01-23 13:06:16 -0500

    .

---
+++ leetcode/_working/1650_loweset_common_ancestor_of_binary_tree_iii.cpp
@@ -0,0 +1,16 @@
+// 1650. Lowest Common Ancestor of a Binary Tree III
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii
+
+// Definition for a node
+class Node {
+ public:
+  int val;
+  Node* left;
+  Node* right;
+  Node* parent;
+};
+
+class Solution {
+ public:
+  Node* lowestCommonAncestor(Node* p, Node* q) {}
+};
