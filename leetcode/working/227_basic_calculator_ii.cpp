ed941eef7a2f9bdf896446346debab399cfaa5f8
smpl <shivajreddy@outlook.com>
2025-01-24 07:49:24 -0500

    .

--- leetcode/working/227_basic_calculator_ii.cpp
+++ leetcode/working/227_basic_calculator_ii.cpp
@@ -14,8 +14,33 @@ Initial Thoughts:
   and convert to int only when we hit a symbol
 
 - BODMAS: Bracket, Order of powers, Division, Multiplication, Addition, Subtraction
+- Main idea of the problem : how to undo  (+ or -) when we encounter (* or /)
+- we should treat + and - as same operation, similary * and /
+  because these operations are inverse of each other
 
+Approach:
+- Instead of using a stack to manage numberes nad operations, we can use variables
+  prev_num : track the last computed value for higher-precedence operations
+- Addition & Subtraction directly update the result.
+- Multiplication & Division modify the 'prev_num' to "undo" their effect
+  when a lower-precedence operation is encountered.
+
+Core Idea:
+   - The main challenge is handling the precedence of * and / over + and -
+   - To achieve this:
+     - Treat + and - as a single operation (low precedence).
+     - Treat * and / as a single operation (high precedence).
+   - When encountering * or /, adjust 'prev_num' without immediately updating the 'result'.
+   - For + and -, finalize the previous operation by adding 'prev_num' to 'result' and resetting 'prev_num'.
+
+
+Processing the String:
+   - Iterate through the string, handling characters based on their type:
+     - Digit: Parse and accumulate the current number.
+     - Operator: Perform the appropriate calculation based on the current operation.
+     - End of String: Add the last number (`prev_num`) to the result.
 */
+
 #include <cctype>
 class Solution {
  public:
@@ -31,9 +56,9 @@ class Solution {
         curr_num = 0;  // Reset current number
         while (idx < s.length() && isdigit(s[idx])) {
           curr_num = curr_num * 10 + (s[idx] - '0');
-          ++idx;
+          idx++;
         }
-        --idx;  // Adjust for the loop increment
+        idx--;  // Adjust for the loop increment
       }
 
       // If the character is an operator or it's the last character
@@ -43,7 +68,7 @@ class Solution {
             result += prev_num;   // Add the previous number to the result
             prev_num = curr_num;  // Update previous number
             break;
-          case '-':  // if -ve treat as +ve
+          case '-':  // -ve treat as +ve
             result += prev_num;
             prev_num = -curr_num;
             break;
