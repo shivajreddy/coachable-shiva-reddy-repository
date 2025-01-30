d38066458f6b6d0a5a5c8f020fdd702e4dc453e6
shiva <shivajreddy@outlook.com>
2025-01-30 10:59:24 -0500

    .

---
+++ leetcode/jan_26_feb_01/791_custom_sort_string.cpp
@@ -0,0 +1,31 @@
+// 791. Custom Sort String
+// https://leetcode.com/problems/custom-sort-string
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Solution {
+ public:
+  string customSortString(string order, string s) {
+    string result;
+    return result;
+  }
+};
+
+int main() {
+
+  auto run_test = [](Solution sol, string order, string s, string expected) {
+    std::cout << "EXPECTED: " << expected << "\n";
+    std::cout << "RECEIVED: " << sol.customSortString(order, s);
+    std::cout << "\n" << std::endl;
+  };
+
+  Solution sol;
+
+  run_test(sol, "cba", "abcd", "cbad");
+  run_test(sol, "bcafg", "abcd", "bcad");
+
+  return 0;
+}
