04638b7992a6f9e332a0eb399d51c2c6ede4f790
shiva <shivajreddy@outlook.com>
2025-03-04 16:04:09 -0500

    [ignore] cleanup

--- DSA/benchmark_2.cpp
+++ DSA/benchmark_2.cpp
@@ -30,39 +30,102 @@ delete methods.
 // Implement a Stack class with push, pop, peek operations.
 // Everything should be implemented in O(1) worst-case runtime using a singly
 // linked list to implement it.
+#include <cstdarg>
+#include <cstddef>
+#include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
 
-// LINKED LIST NODE
 //------------------------------------------
-struct ListNode {
+// SINGLY LINKED LIST NODE
+//------------------------------------------
+struct SLLNode {
   int val;
-  ListNode* next;
+  SLLNode* next;
 
-  ListNode(int val) : val(val), next(nullptr) {}
-  ListNode(int val, ListNode* next) : val(val), next(next) {}
+  SLLNode(int val) : val(val), next(nullptr) {}
+  SLLNode(int val, SLLNode* next) : val(val), next(next) {}
 };
 
+//------------------------------------------
+// SINGLY LINKED LIST
+//------------------------------------------
+struct SLList {
+  SLLNode* head;
+  SLLNode* tail;
+  size_t size; // total node in list
+
+  SLList() : head(nullptr), tail(nullptr), size(0) {}
+
+  ~SLList() {
+    SLLNode* curr = head;
+    while (curr != nullptr) {
+      SLLNode* next = curr->next;
+      delete curr;
+      curr = next;
+    }
+  }
+
+  void push_back(int val) {
+    SLLNode* node = new SLLNode(val);
+    if (size == 0) {
+      head = node;
+      tail = node;
+    } else {
+      tail->next = node;
+      tail = node;
+    }
+    size++;
+  }
+
+  void pop_front() {
+    if (size == 0) return;
+    SLLNode* target = head;
+    head = head->next;
+    delete target;
+    size--;
+    if (size == 1) head->next = nullptr;
+  }
+
+  void push_front(int val) {
+    SLLNode* node = new SLLNode(val);
+    if (size == 0) {
+      head = node;
+      tail = node;
+    } else {
+      SLLNode* old_head = head;
+      node->next = old_head;
+      head = node;
+    }
+    size++;
+  }
+
+  SLLNode* peek_front() { return head; }
+
+  SLLNode* peek_back() { return tail; }
+};
+
+//------------------------------------------
 // STACK
 //------------------------------------------
 /*
-Implementation Details
- - Last In First Out
- - Underlying DataStructure: uses a singly linked list
- - Top item is the head of the linked list
- - New item gets added before the head, making the new node as the head
+   Implementation Details:
+   - Last In First Out
+   - Underlying DataStructure: uses a singly linked list
+   - Top item is the head of the linked list
+   - New item gets added before the head, making the new node as the head
 */
-
 struct Stack {
-  ListNode* top;
+  SLLNode* top;
   size_t size;
 
   Stack() : top(nullptr), size(0) {}
+
   ~Stack() {
     // Remove all nodes
-    ListNode* node = this->top;
+    SLLNode* node = this->top;
     while (node != NULL) {
-      ListNode* next_node = node->next;
+      SLLNode* next_node = node->next;
       delete node;
       node = next_node;
     }
@@ -70,7 +133,7 @@ struct Stack {
 
   // Time: O(1)
   void push(int val) {
-    ListNode* node = new ListNode(val); // Create a new node
+    SLLNode* node = new SLLNode(val); // Create a new node
     node->next = this->top; // new node's next points to current top node
     this->top = node;       // set the new node as the top of stack
     this->size++;
@@ -80,7 +143,7 @@ struct Stack {
   bool pop() {
     if (this->size == 0) return false;
 
-    ListNode* node = this->top;
+    SLLNode* node = this->top;
 
     this->top = node->next;
     delete node; // DeAllocate the node
@@ -91,10 +154,10 @@ struct Stack {
   }
 
   // Time: O(1)
-  ListNode* peek() { return this->top; }
+  SLLNode* peek() { return this->top; }
 
   void print() {
-    ListNode* curr = this->top;
+    SLLNode* curr = this->top;
     while (curr != NULL) {
       printf("%d -> ", curr->val);
       curr = curr->next;
@@ -104,11 +167,135 @@ struct Stack {
 };
 
 //------------------------------------------
+// QUEUE
+//------------------------------------------
+// - Implement a Queue class with enqueue, dequeue, peek operations all in O(1).
+// It should use a singly linked list as well.
+
+// Doubly Linked List Node
+struct DLLNode {
+  int val;
+  DLLNode* next;
+  DLLNode* prev;
+  DLLNode(int val) : val(val), next(nullptr), prev(nullptr) {}
+};
+
+struct DLList {
+  public:
+  DLLNode* head; // Handle Node
+  DLLNode* tail; // Handle Node
+  size_t size;   // number of nodes in DLList  excluding handle nodes
+
+  // Constructor
+  DLList() {
+    head = new DLLNode(-1);
+    tail = new DLLNode(-1);
+    head->next = tail; // H  -> T
+    tail->prev = head; // H <-> T
+    size = 0;
+  }
+
+  // DeConstructor
+  ~DLList() {
+    DLLNode* curr = head->next;
+    while (curr != nullptr) {
+      DLLNode* next = curr->next;
+      delete curr;
+      curr = next;
+    }
+    delete head;
+    delete tail;
+  }
+
+  void push_front(int val) {
+    create_and_insert_between_nodes(val, head, head->next);
+  }
+
+  void push_back(int val) {
+    create_and_insert_between_nodes(val, tail->prev, tail);
+  }
+
+  DLLNode* peek_front() {
+    if (size == 0) return nullptr;
+    return head->next;
+  }
+
+  void pop_front() {
+    // Remove the node after head node
+    remove_next_node(head);
+  }
+
+  void pop_back() {
+    // node <-> target <-> tail
+    auto target = tail->prev;
+    auto node = target->prev; // null for an empty list
+    remove_next_node(node);
+  }
+
+  void print() {
+    DLLNode* curr = head->next;
+    printf("NULL <-> ");
+    for (size_t i = 0; i < size; i++) {
+      printf("%d <-> ", curr->val);
+      curr = curr->next;
+    }
+    printf("NULL\n");
+  }
+
+  private:
+  void create_and_insert_between_nodes(int val, DLLNode* a, DLLNode* b) {
+    // Condition: A <-> B
+    DLLNode* node = new DLLNode(val); // A <-> B | node
+    node->next = b;                   // node  -> B
+    b->prev = node;                   // node <-> B
+    a->next = node;                   // A  -> node <-> B
+    node->prev = a;                   // A <-> node <-> B
+    size++;                           // increase size
+  }
+
+  private:
+  void remove_next_node(DLLNode* node) {
+    if (size == 0 || node == nullptr || node->next == tail)
+      throw std::runtime_error("ValueError: No nodes in DLL to remove");
+    // H ... node <-> target <-> next ... T
+    DLLNode* target = node->next; // node <-> target
+    DLLNode* next = target->next; // node <-> target <-> next
+    node->next = next;            // node  -> next
+    next->prev = node;            // node <-> next
+    delete target;                // DeAllocate the node
+    size--;                       // reduce size
+  }
+};
+
+struct Queue {
+  SLList* sll;
+  size_t size;
+
+  Queue() {
+    sll = new SLList();
+    size = 0;
+  }
+
+  ~Queue() { delete sll; }
+
+  bool enque(int val) {
+    sll->push_back(val); // could throw false if malloc fails
+    size++;
+    return true;
+  }
+
+  int deque() {
+    DLLNode* node = dll->peek_front();
+    dll->pop_front();
+    size--;
+    return node->val;
+  }
+
+  void print() { dll->print(); }
+};
 
 int main() {
   Stack* stack = new Stack();
-  printf("%p \n", (void*)stack);
-  stack->print();
 
   stack->push(10);
   stack->print();
@@ -122,6 +309,13 @@ int main() {
 
   stack->push(40);
   stack->print();
+  printf("-------------------------------\n");
 
-  printf("%p \n", (void*)stack);
+  Queue* q = new Queue();
+  q->enque(10);
+  q->enque(20);
+  q->print();
+  q->deque();
+  q->enque(30);
+  q->print();
 }
