f3cd682f81d855d61e300b18dbe5f434b818f82b
shiva <sreddy@hey.com>
2025-03-12 21:51:11 -0400

    .

--- DSA/hashmap.c
+++ DSA/hashmap.c
@@ -19,15 +19,25 @@ Implement Hashmap
  - Extend your Hashmap to track the smallest and largest keys efficiently
    without scanning all keys on each request.
 */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_KEY_LEN 256
+#define TABLE_SIZE 10
 
 typedef struct HashMap {
   char* key;
   int val;
 } HashMap;
 
-HashMap* create_hashmap() {
+typedef struct Pair {
+  char key[MAX_KEY_LEN];
+  int val;
+} Pair;
+
+HashMap* init_hashmap() {
   HashMap* hm = malloc(sizeof(HashMap));
   if (!hm) {
     printf("Allocating for hashmap failed\n");
@@ -36,11 +46,32 @@ HashMap* create_hashmap() {
   return hm;
 }
 
+unsigned int hash(char* key) {
+  unsigned int hash_val = 0;
+  for (int i = 0; i < strlen(key); i++) {
+    hash_val += key[i];
+    hash_val = (hash_val * key[i]) % TABLE_SIZE;
+  }
+  return hash_val;
+}
+
 int main() {
-  HashMap* hm = create_hashmap();
+  printf("Jacob => %u\n", hash("Jacob"));
+  printf("Natalie => %u\n", hash("Natalie"));
+  printf("Sara => %u\n", hash("Sara"));
+  printf("Mpho => %u\n", hash("Mpho"));
+  printf("Tebogo => %u\n", hash("Tebogo"));
+  printf("Ron => %u\n", hash("Ron"));
+  printf("Jane => %u\n", hash("Jane"));
+  printf("Maren => %u\n", hash("Maren"));
+  printf("Bill => %u\n", hash("Bill"));
+
+  printf("TESTING HASHMAP :: STARTED\n");
+  HashMap* hm = init_hashmap();
   if (!hm) {
     printf("Allocating for hashmap failed\n");
     return EXIT_FAILURE;
   }
+  printf("TESTING HASHMAP :: FINISHED\n");
   return EXIT_SUCCESS;
 }
