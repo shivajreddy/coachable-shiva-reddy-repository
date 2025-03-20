7647e5b57eecbeafd31c4d255e34454b9816c43d
shiva <shivajreddy@outlook.com>
2025-03-20 16:32:45 -0400

    .

--- DSA/benchmark_4.py
+++ DSA/benchmark_4.py
@@ -31,7 +31,8 @@ Insertion Sort
   A: 
   - Describe qualitatively how an array will look at each intermediate stage of insertion sort.
   A: 
-  - Implement insertion sort and modify it to count how many times elements are shifted. How does this relate to its runtime?
+  - Implement insertion sort and modify it to count how many times elements are shifted. How does 
+  this relate to its runtime?
   A: 
 """
 
@@ -79,40 +80,6 @@ sorted_list = insertion_sort(input_list)
 print("EASYEXAMPLE")
 print(''.join(sorted_list))
 
-"""
-Merge Sort
-"""
-
-def merge(arr, aux, lo, mid, hi):
-    for k in range(lo, hi+ 1):
-        aux[k] = arr[k]
-
-    i, j = lo, mid + 1
-    for k in range(lo, hi + 1):
-        if i > mid or aux[j] < aux[i]:
-            arr[k] = aux[j]
-            j += 1
-        else:
-            arr[k] = aux[i]
-            i += 1
-
-# Top-Down
-def mergesort_toopdown(arr: List[int], aux: List[int], lo: int, hi: int):
-    if hi <= lo:
-        return
-    mid = lo + (hi - lo) // 2
-    mergesort_toopdown(arr, aux, lo, mid)
-    mergesort_toopdown(arr, aux, mid+1, hi)
-    merge(arr, aux, lo, mid, hi)
-
-def sort_topdown(arr: List[int]):
-    aux = arr.copy()
-    mergesort_toopdown(arr, aux, 0, len(arr) - 1)
-
-input_list = [3, 7, 1, 2, 18, -5, 0, 12]
-print(input_list)
-sort_topdown(input_list)
-print(input_list)
 
 
 """
@@ -128,16 +95,58 @@ Sorting
 """
 Mergesort
   - Describe how mergesort works at a high-level.
-  A: 
+  A: Core objective is two fold. First splitting the entire input into smaller and smaller 
+  sub problems, where sub problems are inputs that are already sorted. so the base case being a 
+  single item. The second fold is merging two sub-problems (sorted lists) together. This is a 
+  divide & conquer algorithm, it uses extra linear space.
   - What is the runtime? Describe why it’s O(n log n).
-  A: 
+  A: Best case: O(n log n), avg case: O(n log n), worst case O(n log n)
   - Describe qualitatively how an array will look at each intermediate stage of merge sort.
-  A: 
-  - Implement Mergesort.
-  A: 
+  A: input      [ 2, 20, -1, 3, 10, 4, 30, 7]
+               [ 2, 20, -1, 3]    [10, 4, 30, 7]
+              [2, 20]  [-1, 3]   [10, 4]  [30, 7]
+              [2] [20] [-1][3]   [10][4]  [30][7]
+              [2 20]   [-1, 3]   [4 10]   [7 30]
+                [-1 2 3 20]       [4 7 10 30]
+     output          [-1 2 3 4 7 10 20 30]
   - How does merge sort handle duplicates differently than quicksort?
   A: 
 """
+# - Implement Mergesort.
+def merge(arr, aux, lo, mid, hi):
+    aux = [x for x in arr[lo:hi+1]]
+    i, j = lo, mid + 1
+    for k in range(lo, hi + 1):
+        if i > mid:
+            arr[k] = aux[j]
+            j += 1
+        elif j > hi:
+            arr[k] = aux[i]
+            i += 1
+        elif aux[i] <= aux[j]:
+            arr[k] = aux[i]
+            i += 1
+        else:
+            arr[k] = arr[j]
+            j += 1
+
+def mergesort_topdown(arr, aux, lo, hi):
+    if hi <= lo: return
+    mid = lo + (hi - lo) // 2
+    mergesort_topdown(arr, aux, lo, mid)
+    mergesort_topdown(arr, aux, mid + 1, hi)
+    merge(arr, aux, lo, mid, hi)
+
+def merge_sort(arr):
+    aux = arr.copy()
+    mergesort_topdown(arr, aux, 0, len(arr) - 1)
+
+input_list = [3, 7, 1, 2, 18, -5, 0, 12]
+print(input_list)
+merge_sort(input_list)
+print(input_list)
+
+
 
 """
 Quicksort
