5878397f47775bfb57e19d11b92f8b0d5a94c8ea
shiva <shivajreddy@outlook.com>
2025-03-25 08:50:01 -0400

    .

--- leetcode/working/388_longest_absolute_file_path.go
+++ leetcode/working/388_longest_absolute_file_path.go
@@ -2,6 +2,34 @@
 // https://leetcode.com/problems/longest-absolute-file-path
 package main
 
+import (
+	"strings"
+)
+
 func lengthLongestPath(input string) int {
-	return -1
+
+	lines := strings.Split(input, "\n")
+
+	stack := make([]int, len(lines)) // current path lengths
+
+	result := 0 // max width of absolute-path to a file
+
+	for _, line := range lines {
+		tab_count := strings.Count(line, "\t")
+		name := strings.Trim(line, "\t")
+		current_path_width := 0
+		if tab_count > 0 {
+			current_path_width = stack[tab_count-1] + len(name) + 1
+		} else {
+			current_path_width = len(name)
+		}
+
+		stack[tab_count] = current_path_width
+
+		if strings.Contains(name, ".") { // update global result if current path is a file
+			result = max(result, current_path_width)
+		}
+	}
+
+	return result
 }
